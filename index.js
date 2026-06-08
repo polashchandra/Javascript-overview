f61d7005342e9152ff4cbf9d4938fd015cb87312
polashchandra <polashchandra210@gmail.com>
2026-06-08 13:06:12 +0600

    named

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function sweet(){
 const dokan =sweet()
 console.log(dokan)
 //function exprestion
+const matribandar=function(){
+    console.log("sweety")
+}
+//named exprestion function
+const namematribandar=function matribandar(){
+    console.log("sweety")
+}
