52ddf6972f50179bbc6229f4627a24f33ba94816
polashchandra <polashchandra210@gmail.com>
2026-06-08 21:20:50 +0600

    nullsih oparetor

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ const hello=()=>{
     }
 })
 console.log(hello())
+
+funciton hello(){
+    return ()=> {
+        conlolog.log("hello")
+    }
+}
+//nullish undifind
+console.log(null ?? "hello")
+console.log(undefined  || "hello")
+console.log(null && "hello")
