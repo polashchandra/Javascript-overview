a6b4eae9cde7d872801863f786819538b9ac4683
polashchandra <polashchandra210@gmail.com>
2026-06-10 11:40:40 +0600

    true or false

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-// reurular function statement
-function sweet(){
-    console.log ("misty")
-}
-const dokan =sweet()
-console.log(dokan)
-//function exprestion
-const matribandar=function(){
-    console.log("sweety")
-}
-//named exprestion function
-const namematribandar=function matribandar(){
-    console.log("sweety")
-}
-// arrow function
-const hello=()=>{
-    return{
-        a:5,
-        b:6
-    }
-})
-console.log(hello())
+// // reurular function statement
+// function sweet(){
+//     console.log ("misty")
+// }
+// const dokan =sweet()
+// console.log(dokan)
+// //function exprestion
+// const matribandar=function(){
+//     console.log("sweety")
+// }
+// //named exprestion function
+// const namematribandar=function matribandar(){
+//     console.log("sweety")
+// }
+// // arrow function
+// const hello=()=>{
+//     return{
+//         a:5,
+//         b:6
+//     }
+// })
+// console.log(hello())
 
-funciton hello(){
-    return ()=> {
-        conlolog.log("hello")
-    }
-}
-//nullish undifind
-console.log(null ?? "hello")
-console.log(undefined  || "hello")
-console.log(null && "hello")
+// funciton hello(){
+//     return ()=> {
+//         conlolog.log("hello")
+//     }
+// }
+// //nullish undifind
+// console.log(null ?? "hello")
+// console.log(undefined  || "hello")
+// console.log(null && "hello")
 
- api fetch("https://jsonplaceholder.typicode.com/posts")
- .then(res=>res.json())
-.then(data=>console.log(data))
-async function getData() {
-  try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1"
-    );
+//  api fetch("https://jsonplaceholder.typicode.com/posts")
+//  .then(res=>res.json())
+// .then(data=>console.log(data))
+// async function getData() {
+//   try {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/todos/1"
+//     );
 
-    const data = await response.json();
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     const data = await response.json();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+const value=0
+console.log(!!value)
